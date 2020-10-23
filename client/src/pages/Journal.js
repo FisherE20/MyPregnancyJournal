b3ae5a942391c1166f2d5c09ee78d3d0712eecba
Erin Fisher <flybirdie85@gmail.com>
2020-10-22 17:34:22 -0700

    have the original design working again. Need to add functionality

--- client/src/pages/Journal.js
+++ client/src/pages/Journal.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import CreatePostForm from "../components/CreatePostForm";
 import {Col, Row, Container} from "../components/Grid";
-import PostsList from "../components/PostsList";
+
 
 function Journal () {
     return(
@@ -10,7 +10,23 @@ function Journal () {
             <Container fluid>
                 <Row>
                     <Col size="md-6">
-                        <PostsList />
+                        <h1>Journal Entry</h1>
+                        <a class="button primary" href="/post/new">New Post</a>
+
+                        <div class="card mt-4">
+                            <div class="card-body">
+                                <h4 class="card-title">Title</h4>
+                                <div class="card-subtitle text-muted mb-2">
+                                    Date
+                                </div>
+                                <div class="card-text mb-2">Description</div>
+                                <div>
+                                <a class="button primary" href="#">Save</a>
+                                <a class="button success" href="#">Edit</a>
+                                <a class="button alert" href="#">Delete</a>
+                                </div>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
             </Container>
