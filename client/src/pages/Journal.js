6bdacd9909f4c566eb5dd9dcfcb5c8a525192d51
Erin Fisher <flybirdie85@gmail.com>
2020-10-22 17:03:10 -0700

    added file and components. Journal doesn't like API file

--- client/src/pages/Journal.js
+++ client/src/pages/Journal.js
@@ -1,8 +1,21 @@
 import React from "react";
+// import CreatePostForm from "../components/CreatePostForm";
+import {Col, Row, Container} from "../components/Grid";
+import PostsList from "../components/PostsList";
 
 function Journal () {
     return(
-        <h1>Journal Entry</h1>
+        <div>
+            
+            <Container fluid>
+                <Row>
+                    <Col size="md-6">
+                        <PostsList />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+        
     );
 }
 
