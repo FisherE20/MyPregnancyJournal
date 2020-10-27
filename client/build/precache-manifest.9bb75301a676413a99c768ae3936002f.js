3e769808908e1c1f0b7d6f047639a6d4248bd1ff
Erin Fisher <flybirdie85@gmail.com>
2020-10-26 21:38:24 -0700

    Deploying to Heroku

---
+++ client/build/precache-manifest.9bb75301a676413a99c768ae3936002f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e10b88197578416314e",
+    "url": "/static/css/main.c4768686.chunk.css"
+  },
+  {
+    "revision": "8e10b88197578416314e",
+    "url": "/static/js/main.ea5776bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "25cc5ac014b0f9e25d44",
+    "url": "/static/js/2.8d1f72fa.chunk.js"
+  },
+  {
+    "revision": "6a53b7b8d2003ef6364ad8dc28ecb490",
+    "url": "/index.html"
+  }
+];
